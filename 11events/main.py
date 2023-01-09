3f0e4714c22ed7690e49d61a1a08dfebc71d3ab8
Flávio Micheletti <flaviomicheletti@hotmail.com>
2023-01-09 14:57:59 -0300

    events

---
+++ 11events/main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+items = {}
+
+
+@app.on_event("startup")
+async def startup_event():
+    items["foo"] = {"name": "Fighters"}
+    items["bar"] = {"name": "Tenders"}
+
+
+@app.on_event("shutdown")
+def shutdown_event():
+    with open("log.txt", mode="a") as log:
+        log.write("Application shutdown")
+
+
+@app.get("/items/")
+async def read_items():
+    return items
+
+
+@app.get("/items/{item_id}")
+async def read_items(item_id: str):
+    return items[item_id]
+
+
+
+"""
+https://fastapi.tiangolo.com/advanced/events/#events-startup-shutdown
+"""
